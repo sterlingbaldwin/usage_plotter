1c42dc04a65d1782e3bc6de06ae6b481ebe5e843
Sterling Baldwin <baldwin32@llnl.gov>
2021-02-11 10:48:03 -0800

    making progress

--- usage_plotter.py
+++ usage_plotter.py
@@ -4,6 +4,8 @@ import argparse
 from tqdm import tqdm
 from pathlib import Path
 from ipwhois import IPWhois
+import itertools
+from pprint import pprint
 
 
 def parse_args():
@@ -12,7 +14,6 @@ def parse_args():
     return parser.parse_args()
 
 def get_logs(path):
-
     for root, dirs, files in os.walk(path):
         if not files:
             continue
@@ -25,6 +26,7 @@ def filter_lines(path):
     with open(path, 'r') as instream:
         while (line := instream.readline()):
             if "E3SM" in line \
+            and "CMIP6" not in line \
             and "xml" not in line \
             and "ico" not in line \
             and "cmip6_variables" not in line \
@@ -33,6 +35,11 @@ def filter_lines(path):
             and "aggregation" not in line:
                 yield line
 
+
+REALMS = ['ocean', 'atmos', 'land', 'sea-ice']
+DATA_TYPES = ['time-series', 'climo', 'model-output', 'mapping', 'restart']
+
+
 def plot_cmip6(datasets):
     ...
 
@@ -40,11 +47,18 @@ def main():
     parsed_args = parse_args()
     root = parsed_args.root
 
-    datasets = {}
+    datasets = {
+        x: {'requests': 0} for x in itertools.product(REALMS, DATA_TYPES)
+    }
 
     for log in tqdm(get_logs(root)):
         for line in filter_lines(log):
             attrs = line.split()
+            status = attrs[8]
+            # import ipdb; ipdb.set_trace()
+            if "20" not in status:
+                continue
+
             fullpath = attrs[6]
             requester = attrs[0]
             try:
@@ -53,6 +67,26 @@ def main():
                 print("ERROR: " + fullpath)
                 continue
             dataset_id = ".".join(fullpath[idx:].split('/')[:-1])
+
+            try:
+                facets = dataset_id.split('.')
+                realm = facets[4]
+                data_type = facets[6]
+                # import ipdb; ipdb.set_trace()
+                tag = f"{requester}:{dataset_id}"
+                if datasets[(realm, data_type)].get(tag) is None:
+                    datasets[(realm, data_type)]['requests'] += 1
+                    datasets[(realm, data_type)][tag] = 1
+                else:
+                    datasets[(realm, data_type)][tag] += 1
+                
+            except Exception as e:
+                print(e)
+                import ipdb; ipdb.set_trace()
+                ...
+                
+
+
             # if datasets.get(dataset_id) is None:
             #     datasets[dataset_id] = {
             #         requester: 1
@@ -64,7 +98,7 @@ def main():
             #         datasets[dataset_id][requester] += 1
                 
     
-    from pprint import pprint
+    
     import ipdb; ipdb.set_trace()
     pprint(datasets)
 
